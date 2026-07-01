f71615a9f51b50633ff3f436c011391cbbfb0fa2
hlee4786 <hlee4786@usc.edu>
2026-06-30 18:57:23 -0700

    added styling for experience section

--- app/styles/experienceStyles.ts
+++ app/styles/experienceStyles.ts
@@ -5,51 +5,96 @@ const styles = {
     // backgroundColor: "white"
   },
 
-  experienceContainer: {
+  experience1Container: {
     position: "relative" as const,
     width: 600,
     height: 700,
+    // backgroundColor: "white",
+    margin: "0 auto",
   },
 
-  paper1Container: {
+  polaroid: {
     position: "relative" as const,
-    left: -100,
-  }, 
-
-  paper1: {
-    width: 1000,
-    display: "block",
-    filter: "drop-shadow(0 4px 8px rgba(0,0,0,0.15))",
-    transform: "rotate(-3deg)",
+    backgroundColor: "#F0F0F0",
+    padding: "16px 16px 50px 16px",
+    boxShadow: "0 4px 12px rgba(0,0,0,0.45)",
+    display: "inline-block",
+    borderRadius: 2,
   },
 
-  paper1Text: {
+  polaroid2: {
+    left: 280,
+    top: 30,
+  },
+
+  picture: {
+    width: "180px",
+    display: "block",
+  },
+
+  h31: {
     position: "absolute" as const,
+    top: 204,
+    left: 22,
+    fontFamily: "Sophie Comic",
+    fontSize: 36
+  },
+
+  greenPostItContainer: {
+    position: "absolute" as const,
+    left: 190,
+    top: 320,
+    transformOrigin: "center center",
+  },
+
+  greenPostIt: {
+    position: "absolute" as const,
+    left: 190,
+    top: 320,
+    height: 420,
+  },
+
+  greenPostItText: {
+    position: "absolute" as const,
+    top: 80,
+    left: 120,
+    width: 500,
     fontFamily: "Special Elite",
-    top: 116,
-    left: 54,
-    width: 900,
-    marginTop: 30,
-    transform: "rotate(-3deg)",
-    margin: 0,
+    textAlign: "center" as const,
     lineHeight: 1.8,
     fontSize: "1rem",
   },
 
-  illoominate: {
+  yellowPostItContainer: {
     position: "absolute" as const,
-    top: -170,
-    left: -90,
+    left: -160,
+    top: 40,
+    transformOrigin: "center center",
+  },
+
+  yellowPostIt: {
+    position: "absolute" as const,
+    left: -160,
+    top: 40,
+    width: 430,
+  },
+
+  yellowPostItText: {
+    position: "absolute" as const,
+    top: 100,
+    left: 60,
     width: 300,
-    zIndex: 10,
-    transform: "rotate(-8deg)",
+    fontFamily: "Special Elite",
+    textAlign: "center" as const,
+    lineHeight: 1.8,
+    fontSize: "1rem",
   },
 
   aws: {
     position: "absolute" as const,
-    top: 260,
-    right: -370,
-    width: 130,
+    top: 50,
+    left: 620,
+    width: 200,
     zIndex: 10,
     transform: "rotate(-3deg)",
   },
@@ -57,10 +102,26 @@ const styles = {
   typescript: {
     position: "absolute" as const,
     top: 180,
-    right: -455,
-    width: 130,
+    left: 500,
+    width: 200,
     zIndex: 10,
     transform: "rotate(5deg)",
+  },
+
+  blueTicket: {
+    width: 300,
+    position: "absolute" as const,
+    top: 480,
+    left: -100,
+  },
+
+  h41: {
+    position: "absolute" as const,
+    top: 70,
+    left: 96,
+    margin: 0,
+    fontFamily: "Sophie Comic",
+    fontSize: 36,
   }
 }
 
